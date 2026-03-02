34386a7600d7fb9e66f245f5c7a0a37fa90106de
Kauã Gabriell <kauagabriell64@gmail.com>
2026-03-02 12:59:22 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello Word');
+import 'dotenv/config';
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: Number(process.env.PORT ?? 3000) });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
